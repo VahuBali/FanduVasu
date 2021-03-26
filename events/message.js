aea6c3a4831e2a0419b19166d06b1d6b19469539
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-03-26 16:48:52 -0400

    Delete message.js

--- events/message.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'message',
-    execute(message) {
-        console.log(`${message.author.tag} in ${message.guild.name} at the #${message.channel.name} channel, sent: ${message.content}`)
-    }
-}
